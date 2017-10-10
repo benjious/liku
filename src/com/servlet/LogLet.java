55ef6a139eefd7fb86b40cfc41fd3b21381e5412
benjious <893326146@qq.com>
2017-10-10 15:52:23 +0800

    没有使用okhttp  和 JSON 之前

---
+++ src/com/servlet/LogLet.java
@@ -0,0 +1,88 @@
+package com.servlet;
+
+import com.sqlService.SqlOperator;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogLet extends HttpServlet {
+    private static final long serialVersionUID = 369840050351775312L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        username = charSetConvert(username);
+        String password = request.getParameter("password");
+        System.out.println(username + "-----" + password);
+
+
+        //新建服务对象
+        SqlOperator sqlOperator = new SqlOperator();
+
+
+        //验证处理
+        boolean isLogin = sqlOperator.login(username, password);
+        System.out.println("验证处理的结果是：    " + isLogin);
+        if (isLogin) {
+            System.out.println("Success");
+            request.getSession().setAttribute("username", username);
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html");
+            System.out.println("");
+            PrintWriter out = response.getWriter();
+            out.print(username);
+            out.flush();
+            out.close();
+        } else {
+            System.out.println("Failed");
+        }
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+
+    public String charSetConvert(String request) {
+        String charSet = geteEncoding(request);
+        System.out.println("传过来的编码是   ： " + charSet);
+        try {
+            byte[] b = request.getBytes(charSet);
+            request = new String(b, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return request;
+
+    }
+
+    public static String geteEncoding(String str) {
+        String[] charSet = {"GBK", "GB2312", "UTF-8", "ISO-8859-1"};
+        try {
+            for (int i = 0; i < charSet.length; i++) {
+                if (str.equals(new String(str.getBytes(charSet[i]), charSet[i]))) {
+                    String s1 = charSet[i];
+                    return s1;
+
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
+}
+
+
+
---
+++ src/com/sqlService/SqlOperator.java
@@ -0,0 +1,37 @@
+package com.sqlService;
+
+import com.db.DBManager;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ */
+public class SqlOperator {
+    public Boolean login(String username, String password) {
+
+        //获取sql查询语句
+        String logSql = "select * from student where username ='" + username + "' and password ='" + password + "'";
+        System.out.println("  xyz 登录的语句为：" + logSql);
+        //初始化连接数据库对象
+        DBManager manager = DBManager.createInstance();
+        manager.connectDB();
+        //使用对象进行查询
+        try {
+
+            ResultSet rs = manager.executeQuery(logSql);
+            if (rs.next()) {
+                manager.closeDB();
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        manager.closeDB();
+        return false;
+    }
+
+
+}
